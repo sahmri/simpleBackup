435237622e2303dc56fc90346ded939591e8c203
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-06 22:43:36 +0100

    reformatting of update daemon

--- updateme.py
+++ updateme.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-#########################################################################################################################
-#										Simple update shortcut for this repository										#
-#########################################################################################################################
-#																		(C) Manuel Bernal Llinares <mbdebian@gmail.com>	#
-#																			Distributed under the Apache License 2.0	#
-#########################################################################################################################
+########################################################################################################################
+#										Simple update shortcut for this repository									   #
+########################################################################################################################
+#																		(C) Manuel Bernal Llinares <mbdebian@gmail.com>#
+#																			Distributed under the Apache License 2.0   #
+########################################################################################################################
 
 # This script is design to run as a daemon, and periodically update this code
 
@@ -14,21 +14,24 @@ import updateLogger as logger
 import updateSettings
 import subprocess
 
+
 def main():
-	config = updateSettings.getConfig()
-	miLogger = logger.getLogger()
+    config = updateSettings.getConfig()
+    miLogger = logger.getLogger()
 
-	miLogger.info("=" * 120)
-	miLogger.info("Update session started, checking for updates...")
-	command = "git pull >> " + config['logger']['ouputAndErrorFilePath'] + " 2>&1"
-	miLogger.debug("Update command: " + command)
-	p = subprocess.Popen(command, shell=True)
-	if (p.wait()):
-		miLogger.error("An error occurred while running the updates, please, check " + config['logger']['ouputAndErrorFilePath'] + " for details.")
-	else:
-		miLogger.info("Check and update run with success!")
+    miLogger.info("=" * 120)
+    miLogger.info("Update session started, checking for updates...")
+    command = "git pull >> " + config['logger']['ouputAndErrorFilePath'] + " 2>&1"
+    miLogger.debug("Update command: " + command)
+    p = subprocess.Popen(command, shell=True)
+    if (p.wait()):
+        miLogger.error("An error occurred while running the updates, please, check " + config['logger'][
+            'ouputAndErrorFilePath'] + " for details.")
+    else:
+        miLogger.info("Check and update run with success!")
 
-	miLogger.info("=" * 120)
+    miLogger.info("=" * 120)
+
 
 if __name__ == "__main__":
-	main()
+    main()
